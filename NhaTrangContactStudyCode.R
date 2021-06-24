b10064b8949a854a2f26e26ce805a7eeab55af10
Stefan Flasche <Stefan.Flasche@lshtm.ac.uk>
2021-06-24 20:48:32 +0100

    merged a few files
    and shortned the second regression output

--- NhaTrangContactStudyCode.R
+++ NhaTrangContactStudyCode.R
@@ -8,7 +8,7 @@
 #
 # G Qian, 2021 on behalf of the authors of: "Pneumococcal exposure routes for infants, a nested cross-sectional survey in Nha Trang, Vietnam"
 ######################################
-
+set.seed(31415)
 
 ###################################
 ### load libraries
@@ -29,12 +29,9 @@ options(dplyr.summarise.inform = FALSE)
 ### load data
 ###################################
 #Load data from the contact study
-df_contacts <- read.csv("df_contacts_public.csv")
-
-#Define age grouping and arrange infant contacts into these groups
-#Here, age groups are simply 1 year 'bands' (e.g. 1 yr old, 2 yrs etc)
-brks <- 0:100
-df_contacts$ctagegp <- cut(df_contacts$contact_age, brks, right = F)
+df_contacts <- 
+  read.csv("df_contacts_public.csv") %>%
+  mutate(ctagegp = cut(contact_age, 0:100, right = F))
 
 #Load relevant data from the carriage study
 df_infants <- read.csv("df_infants_public.csv")
@@ -45,32 +42,9 @@ df_infants <- read.csv("df_infants_public.csv")
 CarriageDataThesis3 <- read.csv("CarriageDataCarlaHalfYear.csv")
 
 # Carriage prevalence observed in the trial
-RateCarInf46months <- 0.1478873
-RateCarInf68months <- 0.1839378
-RateCarInf810months <- 0.2358722
-RateCarInf1012months <- 0.27897
-RateCarTod1214months <- 0.2769231
-RateCarTod1416months <- 0.352381
-RateCarTod1618months <- 0.3449477
-RateCarTod1820months <- 0.3556851
-RateCarTod2022months <- 0.4134276
-RateCarTod2224months <- 0.4205298
-carriageR <-
-  c(
-    RateCarInf46months,
-    RateCarInf68months,
-    RateCarInf810months,
-    RateCarInf1012months,
-    RateCarTod1214months,
-    RateCarTod1416months,
-    RateCarTod1618months,
-    RateCarTod1820months,
-    RateCarTod2022months,
-    RateCarTod2224months
-  ) #read.csv("CarlaDataCarriageRate.csv")
-age <-  c(5 / 12, 7 / 12, 9 / 12, 11 / 12, 13 / 12, 15 / 12, 17 / 12, 19 /
-            12, 21 / 12, 23 / 12)
-NTrang4thUnder2 <- data.frame(age, carriageR)
+NTrang4thUnder2 <- 
+  tibble (age = c(5, 7, 9, 11, 13, 15, 17, 19, 21, 23)/12,
+          carriageR = c(0.147, 0.183, 0.235, 0.278, 0.276, 0.352, 0.344, 0.355, 0.413, 0.420))
 
 # some data wrangling to get individual level data into format needed by model
 CarriageDataThesis3 <- CarriageDataThesis3 %>%
@@ -95,22 +69,6 @@ CarriageDataThesis3_ <- CarriageDataThesis3 %>%
     names_from = key
   ))) %>%
   unnest(CI)
-
-#Prepare 
-dat <- CarriageDataThesis3_ %>%
-  unnest(data) %>%
-  select(y, N) %>% as.list %>%
-  append(.,
-         values = list(
-           K = ncol(X),
-           Q = Q,
-           X.pred = X.pred,
-           X      = X,
-           n = nrow(X),
-           m = nrow(X.pred),
-           beta.0 = matrix(data = rep(0, ncol(X)),
-                           ncol = 1)
-         ))
 
 ###################################
 ### First Jags Model (cubic spline)
@@ -179,15 +137,32 @@ Q <- makeQ(2, nrow(X))
 attributes(X) <- attributes(X)["dim"]
 attributes(X.pred) <- attributes(X.pred)["dim"]
 
+#Prepare 
+dat <- CarriageDataThesis3_ %>%
+  unnest(data) %>%
+  select(y, N) %>% as.list %>%
+  append(.,
+         values = list(
+           K = ncol(X),
+           Q = Q,
+           X.pred = X.pred,
+           X      = X,
+           n = nrow(X),
+           m = nrow(X.pred),
+           beta.0 = matrix(data = rep(0, ncol(X)),
+                           ncol = 1)
+         ))
+
 #Implement RJags to find the cubic spline fit
 mod <-
   jags.model(file = "spline_model.R",
              data = dat,
              n.adapt = 5e3)
 
-burn <- jags.samples(mod, variable.names = "p.rep", n.iter = 1e4)
-pred <- coda.samples(mod, "p.rep", n.iter = 1e4)
-post <- coda.samples(mod, c("beta", "beta.00"), n.iter = 1e4)
+number_iter = 1000
+burn <- jags.samples(mod, variable.names = "p.rep", n.iter = number_iter)
+pred <- coda.samples(mod, "p.rep", n.iter = number_iter)
+post <- coda.samples(mod, c("beta", "beta.00"), n.iter = number_iter)
 
 # model diagnostics
 statsBeta <- tidy(post)
@@ -214,7 +189,7 @@ Figure2a <- tidy(pred) %>%
                y = p
              )) +
   xlab("Age") +
-  theme_bw() +
+  theme_classic() +
   ylab("Prevalence") +
   scale_x_continuous(trans = "sqrt", #we will use a square root scale on the x-axis to make the lower ages more prominent
                      breaks = c(1:10) ^ 2) +
@@ -222,7 +197,8 @@ Figure2a <- tidy(pred) %>%
   geom_point(data = NTrang4thUnder2,
              aes(age, carriageR),
              colour = "darkgreen",
-             size = sizeDot) + scale_fill_discrete(breaks = c("trt1", "ctrl", "trt2")) + labs(y = "Carriage Prevalence", x = "Age (years)")
+             size = 3) + scale_fill_discrete(breaks = c("trt1", "ctrl", "trt2")) + labs(y = "Carriage Prevalence", x = "Age (years)") 
+  
 Figure2a
 
 
@@ -239,178 +215,45 @@ Figure2a
 #  Calculate PEI (remember, only the prevalence changes across samples of 'post')
 #  Generate df_expFINAL dataframe for each run of for loop
 #  Use as input into the PEI Jags/6
-
 # }
 
-number_iter = 10000
-max_age = 100 #Maximum age of contacts (set to 100 here)
-#pred[[1]] is a matrix [number_iter=10000, max_age=100] containing the runs of the MCMC (10000) and 100 corresponding to age
-pred[[1]][number_iter, max_age]
 
-predStore <- tidy(pred) %>%
-  bind_cols(data.frame(x = x.pred))
-
-AgeVector <- predStore$x
-
-#mcmc_to_dt is a neat function that we can access here: https://rdrr.io/github/njtierney/mmcc/man/mcmc_to_dt.html
-pred_wider <- pred %>%
-  mcmc_to_dt()   %>%
-  pivot_wider(names_from = parameter, values_from = value)
-
-#Calculate PEI for all N:
-
-#First, set up the vectors required
-carriageIter1 = pred_wider[1, -(1:2)]
-carriageIter1ExtVec <- unlist(carriageIter1, use.names = FALSE)
-AllAges <- 0:max_age - 1
-carriageIterVectors <- vector()
-
-#Now, we will use a loop to obtain the values
-for (r in 1:number_iter) {
-  carriageVector <- unlist(pred_wider[r, -(1:2)], use.names = FALSE)
-  carriageIterVectors <- rbind(carriageIterVectors, carriageVector)
-}
-
-#Set up the age bins
-carriageIterVectorsAge <- as.data.frame(t(carriageIterVectors))
-carriageIterVectorsAge$agegroups <-
-  levels(cut(df_contacts$contact_age, brks, right = F))
-storePEIValues <- ((carriageIterVectorsAge))
-
-#change column names now
-#define a list of varying "varname"
-varname <- c('Iteration', 'ctagegp')
-#Find how many times "varname" repeats itself
-n <- c(10000, 1)
-
-#Replace column name
-names(storePEIValues)[1:ncol(storePEIValues)] <-
-  unlist(mapply(function(x, y)
-    paste(x, seq(1, y), sep = "_"), varname, n))
-names(storePEIValues)[10001] <- "ctagegp"
-#Check
-names(storePEIValues)
-
-#Keep the contact age at the beginning of the dataframe
-storePEIValuesRenamed <- storePEIValues %>%
-  select(ctagegp, everything())
-
-#Rename the 1st column (contact's age group)
-names(storePEIValuesRenamed)[1] <- "ctagegp"
-#Check to see if column names make sense
-names(storePEIValuesRenamed)
-
-#Set up another dataframe that gives us the number of contacts of each age for each infant
-#and append this to the previous dataframe
-store_PEI_Age = df_contacts %>%
-  group_by(contact_serial_no , ctagegp) %>% summarise(N = length(contact_age)) %>%
-  inner_join(y = storePEIValuesRenamed, by = "ctagegp")
-
-#First, test by calculating PEI for a single iteration
-ProbNonInfected     <- 1 - store_PEI_Age[, 4]
-ContactsPerAgeGroup <- store_PEI_Age$N
-ProbEscape_df       <- ProbNonInfected ^ ContactsPerAgeGroup
-#This is the probability of 'escape' from contacts in each age group
-store_PEI_Age['escape'] <- ProbEscape_df
-#Now specify the chance of having at least 1 effective contact (i.e. PEI)
-ProbExposure_df <- store_PEI_Age %>%
-  group_by(contact_serial_no)   %>%
-  summarise(exp.prop = 1 - prod(escape))
-
-#Now calculate all PEI values
-ProbExposureStore_df <-
-  as.data.frame(ProbExposure_df$contact_serial_no)
-names(ProbExposureStore_df [1]) <- "ctslno"
-gc()
-
-store_PEI_Age %>%
-  pivot_longer(!c("contact_serial_no", "ctagegp", "N"),
-               names_to = "Posterior",
-               values_to = "carriagePrev") %>%
-  mutate(escape = (1 - carriagePrev) ^ N) %>%
-  group_by(contact_serial_no) %>% summarise(exp.prop = 1 - prod(escape)) -> ProbExposureStore_df
-
-#For loop that calculates PEI by taking number_iter samples of carriage rates from the spline fit above
-for (i in 1:number_iter) {
-  df_tmp = store_PEI_Age[, c(1, 2, 3, i + 3)]
-  ProbNonInfected     <- 1 - df_tmp[, 4]
-  ContactsPerAgeGroup <- df_tmp$N
-  ProbEscape_df       <- ProbNonInfected ^ ContactsPerAgeGroup
+pred_prevalence <- 
+  pred %>%
+  mcmc_to_dt() %>%
+  pivot_wider(names_from = iteration, values_from = value, names_prefix = "post ") %>%
+  mutate(ctagegp = cut(0:99,breaks = 0:100, right=F), .before="post 1") %>%
+  select(-c(chain,parameter)) 
   
-  df_tmp['escape'] <- ProbEscape_df[1]
-  
-  tempo <- df_tmp %>%
-    group_by(contact_serial_no)   %>%
-    summarise(exp.prop = 1 - prod(escape))
-  
-  #ProbExposureStore_df$PEI[i]<-tempo
-  
-  ProbExposureStore_df <- cbind(ProbExposureStore_df, tempo[, 2])
-}
+PEI <-
+  df_contacts %>%
+  tibble() %>%
+  select(-c(X, contact_serial_no, contact_age)) %>%
+  left_join(pred_prevalence, by = "ctagegp") %>%
+  pivot_longer(cols = starts_with("post"), names_to = "posterior") %>%
+  group_by(infant_serial_no, posterior) %>% summarise(PEI = 1 - prod(1-value)) 
 
-#We notice that ProbExposureStore_df is a dataframe whose dimensions are 1583 by number_iter+1
+Figure2b <- 
+  PEI %>%
+  left_join(df_infants[,c("infant_serial_no","infant_pneumo_status")], by = "infant_serial_no") %>%
+  filter(!is.na(infant_pneumo_status)) %>%
+  ggplot(aes(x = as.factor(infant_pneumo_status), y = PEI)) +
+    geom_violin(fill="lightblue") +
+    geom_boxplot(width=.07, fill="orange") +
+    ylab("PEI value") + xlab("") +
+    scale_y_continuous(labels = scales::percent) +
+    scale_x_discrete(labels=c("0" = "non-carriers", "1" = "carriers")) +
+    theme_classic() 
 
-#Change column names now (makes things easier to handle later)
-#Define a list of varying "varname"
-varname <- c('contact_serial_no', 'PEI')
-#define how many times above "varname" repeat itself
-n <- c(1, 10000)
+Figure2 <-
+  Figure2a + 
+  annotation_custom(ggplotGrob(Figure2b),
+                    xmin = 3.1,
+                    xmax = 10,
+                    ymin = .38,
+                    ymax = .85)
+ggsave("Figures/Figure2.pdf", unit = "cm", width=20, height=12)                    
 
-#Replace column name
-names(ProbExposureStore_df)[2:ncol(ProbExposureStore_df)] <-
-  unlist(mapply(function(x, y)
-    paste(x, seq(1, y), sep = "_"), varname, n))
-#Drop the first column (lots of names)
-ProbExposureStoreOnlyNum <- ProbExposureStore_df[-c(1)]
-#Calculate the mean PEI value for each individual (i.e. calculate row mean)
-MeanPEI <- rowMeans(ProbExposureStoreOnlyNum, na.rm = TRUE)
-#Calculate the standard deviation of the PEI values
-stdPEI <- apply(ProbExposureStoreOnlyNum, 1, sd)
-#Median of individuals' PEI
-medianPEI <- apply(ProbExposureStoreOnlyNum, 1, median)
-
-
-
-#Append the mean and standard deviation of each infant's PEI values to their carriage status
-df_infants_PEI <- data.frame(df_infants, MeanPEI, stdPEI)
-
-
-ProbExposureStoreOnlyNumReshape <-
-  melt(ProbExposureStoreOnlyNum)  #the function melt reshapes it from wide to long
-ProbExposureStoreOnlyNumReshape$rowid <- 1:1583
-#Inspect the first 10 rows
-head(ProbExposureStoreOnlyNumReshape, 10)
-
-#Take a look at one individual's PEI values by plotting them with a histogram
-PEI1 <- ProbExposureStoreOnlyNumReshape %>% slice(1:10001)
-ggplot(PEI1, aes(x = value)) + geom_histogram(binwidth = 0.1)
-# We can also use a boxplot
-pPEI1 <- ggplot(PEI1 %>% slice(1:1000), aes(x = variable, y = value)) +
-  geom_violin()
-pPEI1  +
-  geom_boxplot(width = 0.1, fill = "orange") +
-  theme(strip.background = element_blank(),) + ylab("PEI value") + xlab("Individual A") +
-  theme(
-    plot.title = element_text(hjust = 0.5),
-    #axis.title.x=element_blank(Raccoon),
-    axis.text.x = element_blank(),
-    axis.ticks.x = element_blank(),
-    strip.text = element_text(size =
-                                15),
-    text = element_text(size = 15)
-  ) +
-  scale_y_continuous(limits = c(0, 1)) + theme(
-    # Hide panel borders and remove grid lines
-    panel.border = element_blank(),
-    # Remove panel grid lines
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    # Remove panel background
-    panel.background = element_blank(),
-    # Add axis line
-    axis.line = element_line(colour = "black")
-    
-  )
 
 
 ###########################################################################
@@ -419,16 +262,25 @@ pPEI1  +
 
 #Read data in preparation for Rjags -> collect these in a list called 'jdat'
 
+PEI_av <-
+  PEI %>% 
+  group_by(infant_serial_no) %>% summarise(m = mean(PEI, na.rm = T),
+                                           s = sd(PEI, na.rm = T)) %>%
+  left_join(df_infants, by = "infant_serial_no") %>%
+  filter(m>0) %>%
+  mutate(commune_id = as.factor(commune_id))
+
 jdat <-
   list(
-    "pneu_carr" = df_infants_PEI$infant_pneumo_status,
-    "mu" = df_infants_PEI$MeanPEI,
-    "sigma" = df_infants_PEI$stdPEI
+    "pneu_carr" = PEI_av$infant_pneumo_status,
+    "mu" = PEI_av$m,
+    "sigma" = PEI_av$s,
+    "age"= PEI_av$infant_age_months,
+    "commune" = PEI_av$commune_id
   )
 
 #Define model
 jcode <- "model{
-
 	#Likelihood
 	#As we have assumed a beta distribution for PEI (exp.prop), we need to specify a[i] and b[i] appropriately
 	for (i in 1:length(pneu_carr)){
@@ -438,12 +290,26 @@ jcode <- "model{
     #Now, exp.prop will take a beta distribution
     exp.prop[i] ~ dbeta(a[i], b[i])
     #And finally, specify the logistic regression equation for infant carriage
-    logit(prob_carr[i]) =  beta0 + beta1 * exp.prop[i]
+    logit(prob_carr[i]) =  beta0 + beta1 * exp.prop[i] + beta2[commune[i]] + beta3 * age[i]
 	}
-
-	#priors
+	#Define our priors
   beta0 ~ dnorm(0,1)
   beta1 ~ dnorm(0,1)
+  
+  #There are 27 communes that we include in the contact study and so...
+  
+  for(i in 1:27) {
+      beta2[i] ~  dnorm(beta2_mu, beta2_sigma)
+  }
+
+  #If we include beta0, then beta2_mu by definition should be 0 or we get identifiability issues
+  beta2_mu <- 0
+  #We assume the standard deviation follows a gamma distribution 
+  beta2_sigma ~ dgamma(1,1)
+
+  #Prior for beta3 (coefficient of infant age)
+  beta3 ~ dnorm(0,1)
+  
 }"
 
 
@@ -456,11 +322,8 @@ jmod = jags.model(
   n.adapt = 1000
 )
 update(jmod)
-jpos = coda.samples(jmod, c("beta0", "beta1"), n.iter = mcmc.length)
-plot(jpos) # check convergence
+jpos = coda.samples(jmod,  c("beta0","beta1","beta2","beta3"), n.iter = mcmc.length)
 
-#Show the coefficients of the logistic regression
-print(beta0_est <-
-        jpos[[1]][, "beta0"] %>% quantile(probs = c(.5, .025, .975)))
-print(beta1_est <-
-        jpos[[1]][, "beta1"] %>% quantile(probs = c(.5, .025, .975)))
+# show regression coefficients
+tidy(jpos)
+
