065b60d6375a018d598135b942eb1090e50dbc36
GeorgeYQian <george.qian08@alumni.imperial.ac.uk>
2021-07-09 15:57:22 +0100

    Removed redundant B-Spline code

--- NhaTrangContactStudyCode.R
+++ NhaTrangContactStudyCode.R
@@ -96,43 +96,6 @@ ggsave("Figures/Figure1.pdf", unit = "cm", width=15, height=9)
 ### First Jags Model (cubic spline)
 ###################################
 
-bspline <-
-  function(x,
-           K,
-           bdeg = 3,
-           cyclic = FALSE,
-           xl = min(x),
-           xr = max(x)) {
-    x <- as.matrix(x, ncol = 1)
-    
-    ndx <- K - bdeg
-    
-    # p-spline algorithm, as outlined in Eilers and Marx (1996)
-    dx <- (xr - xl) / ndx
-    t <- xl + dx * (-bdeg:(ndx + bdeg))
-    T <- (0 * x + 1) %*% t
-    X <- x %*% (0 * t + 1)
-    P <- (X - T) / dx
-    B <- (T <= X) & (X < (T + dx))
-    r <- c(2:length(t), 1)
-    
-    for (k in 1:bdeg) {
-      B <- (P * B + (k + 1 - P) * B[, r]) / k
-      
-    }
-    
-    B <- B[, 1:(ndx + bdeg)]
-    
-    if (cyclic == 1) {
-      for (i in 1:bdeg) {
-        B[, i] <- B[, i] + B[, K - bdeg + i]
-      }
-      B <- B[, 1:(K - bdeg)]
-    }
-    
-    return(B)
-  }
-
 K = 6
 knots <- c(0, 3, 6, 9, 12, 18, 24)
 
