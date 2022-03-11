6a27051eece5853db99bd4864b3b6d069e32c827
Sam Clifford <sj.clifford@gmail.com>
2022-03-11 10:58:43 +0000

    new colour scheme to make Long contacts more visible

--- NhaTrangContactStudyCode.R
+++ NhaTrangContactStudyCode.R
@@ -40,8 +40,8 @@ if (!require(mmcc)){
 ###################################
 # Load data from the contact study
 df_contacts <- 
-  read.csv("contacts_public.csv") %>%
-  mutate(ctagegp = cut(contact_age, 0:100, right = F))
+    read.csv("contacts_public.csv") %>%
+    mutate(ctagegp = cut(contact_age, 0:100, right = F))
 
 # Load relevant data from the carriage study
 df_infants <- read.csv("infants_public.csv")
@@ -53,32 +53,32 @@ CarriageDataThesis3 <- read.csv("Talarico.csv")
 
 # Carriage prevalence observed in the trial
 NTrang4thUnder2 <- 
-  tibble (age = c(5, 7, 9, 11, 13, 15, 17, 19, 21, 23)/12,
-          carriageR = c(0.147, 0.183, 0.235, 0.278, 0.276, 0.352, 0.344, 0.355, 0.413, 0.420))
+    tibble (age = c(5, 7, 9, 11, 13, 15, 17, 19, 21, 23)/12,
+            carriageR = c(0.147, 0.183, 0.235, 0.278, 0.276, 0.352, 0.344, 0.355, 0.413, 0.420))
 
 # some data wrangling to get individual level data into format needed by model
 CarriageDataThesis3 <- CarriageDataThesis3 %>%
-  rename(CarriageStatus = CarriageStatusH,
-         AllAges = AllAgesH)
+    rename(CarriageStatus = CarriageStatusH,
+           AllAges = AllAgesH)
 
 CarriageDataThesis3_ <- CarriageDataThesis3 %>%
-  count(AllAges, CarriageStatus) %>%
-  pivot_wider(names_from = CarriageStatus,
-              values_from = n,
-              values_fill = 0) %>%
-  mutate(N = `0` + `1`) %>%
-  mutate(y = `1`,
-         p = y / N) %>%
-  nest(data = c(y, N)) %>%
-  mutate(CI = map(data, ~ prop.test(.x$y, .x$N)$conf.int)) %>%
-  mutate(CI = map(CI, ~ data.frame(
-    value = .x, key = c("L", "U")
-  ))) %>%
-  mutate(CI = map(CI, ~ pivot_wider(
-    .x, values_from  = value,
-    names_from = key
-  ))) %>%
-  unnest(CI)
+    count(AllAges, CarriageStatus) %>%
+    pivot_wider(names_from = CarriageStatus,
+                values_from = n,
+                values_fill = 0) %>%
+    mutate(N = `0` + `1`) %>%
+    mutate(y = `1`,
+           p = y / N) %>%
+    nest(data = c(y, N)) %>%
+    mutate(CI = map(data, ~ prop.test(.x$y, .x$N)$conf.int)) %>%
+    mutate(CI = map(CI, ~ data.frame(
+        value = .x, key = c("L", "U")
+    ))) %>%
+    mutate(CI = map(CI, ~ pivot_wider(
+        .x, values_from  = value,
+        names_from = key
+    ))) %>%
+    unnest(CI)
 
 
 #######################################
@@ -86,18 +86,23 @@ CarriageDataThesis3_ <- CarriageDataThesis3 %>%
 #######################################
 
 df_contacts %>%
-  group_by(contact_age,contact_time) %>% 
-  summarise(N = length(contact_age)) %>%
-  ggplot(aes(x = contact_age, 
-             y = N, 
-             fill = factor(contact_time))) +
-  geom_bar(stat="identity")+
-  xlab("Contact Age (years)") + ylab("Number of Contacts") + 
-  scale_fill_brewer(palette = "Accent", 
-                    name = "Contact Time", 
-                    labels = c("Short (<5 minutes)", "Medium (5-60 minutes)", "Long (>60 minutes)")) +  
-  theme_classic() +
-  theme(legend.position = c(0.8, 0.8)) 
+    group_by(contact_age,contact_time) %>% 
+    summarise(N = length(contact_age)) %>%
+    ggplot(aes(x = contact_age, 
+               y = N, 
+               fill = factor(contact_time),
+               group = factor(contact_time))) +
+    geom_bar(stat="identity")+
+    xlab("Contact Age (years)") + 
+    ylab("Number of Contacts") + 
+    scale_fill_manual(values  = rev(tail(RColorBrewer::brewer.pal(4,"Reds"),-1)), 
+                      name    = "Contact Time", 
+                      labels  = c("Long (>60 minutes)",
+                                  "Medium (5-60 minutes)",
+                                  "Short (<5 minutes)")) +  
+    theme_classic() +
+    theme(legend.position = c(0.8, 0.8)) 
+
 ggsave("Figures/Figure1.pdf", unit = "cm", width=15, height=9)
 
 
@@ -109,49 +114,49 @@ K = 6
 knots <- c(0, 3, 6, 9, 12, 18, 24)
 
 X <- bs(
-  x = unlist(CarriageDataThesis3_$AllAges),
-  knots = knots,
-  intercept = FALSE
+    x = unlist(CarriageDataThesis3_$AllAges),
+    knots = knots,
+    intercept = FALSE
 )
 
 x.pred <- seq(0, 99, length.out = 100)
 
-X.pred <- bs(x = x.pred,
-             knots = knots,
+X.pred <- bs(x         = x.pred,
+             knots     = knots,
              intercept = FALSE)
 
 makeQ = function(degree, K, epsilon = 1e-3) {
-  x <- diag(K)
-  E <- diff(x, differences = degree)
-  return(t(E) %*% E + x * epsilon)
+    x <- diag(K)
+    E <- diff(x, differences = degree)
+    return(t(E) %*% E + x * epsilon)
 }
 
 Q <- makeQ(2, nrow(X))
 
-attributes(X) <- attributes(X)["dim"]
+attributes(X)      <- attributes(X)["dim"]
 attributes(X.pred) <- attributes(X.pred)["dim"]
 
 #Prepare 
 dat <- CarriageDataThesis3_ %>%
-  unnest(data) %>%
-  select(y, N) %>% as.list %>%
-  append(.,
-         values = list(
-           K = ncol(X),
-           Q = Q,
-           X.pred = X.pred,
-           X      = X,
-           n = nrow(X),
-           m = nrow(X.pred),
-           beta.0 = matrix(data = rep(0, ncol(X)),
-                           ncol = 1)
-         ))
+    unnest(data) %>%
+    select(y, N) %>% as.list %>%
+    append(.,
+           values = list(
+               K = ncol(X),
+               Q = Q,
+               X.pred = X.pred,
+               X      = X,
+               n = nrow(X),
+               m = nrow(X.pred),
+               beta.0 = matrix(data = rep(0, ncol(X)),
+                               ncol = 1)
+           ))
 
 #Implement RJags to find the cubic spline fit
 mod <-
-  jags.model(file = "spline_model.R",
-             data = dat,
-             n.adapt = 5e3)
+    jags.model(file = "spline_model.R",
+               data = dat,
+               n.adapt = 5e3)
 
 number_iter = 1000
 burn <- jags.samples(mod, variable.names = "p.rep", n.iter = number_iter)
@@ -163,33 +168,33 @@ statsBeta <- tidy(post)
 
 # Now we can plot the spline
 Figure2a <- tidy(pred) %>%
-  bind_cols(data.frame(x = x.pred)) %>%
-  ggplot(data = ., aes(x = x, y = mean)) +
-  geom_ribbon(aes(ymin = `2.5%`,
-                  ymax = `97.5%`),
-              alpha = 0.5,
-              fill = "lightskyblue") +
-  geom_line() +
-  geom_point(data = CarriageDataThesis3_,
-             aes(x = AllAges, y = p),
-             color = "red",
-             size = 3) + geom_linerange(data = CarriageDataThesis3_, aes(
-               x = AllAges,
-               ymin = L,
-               ymax = U,
-               y = p
-             )) +
-  xlab("Age") +
-  theme_classic() +
-  ylab("Prevalence") +
-  scale_x_continuous(trans = "sqrt", #we will use a square root scale on the x-axis to make the lower ages more prominent
-                     breaks = c(1:10) ^ 2) +
-  geom_point(data = data.frame(x = knots, mean = 0), color = "grey") + xlab("Age") +
-  geom_point(data = NTrang4thUnder2,
-             aes(age, carriageR),
-             colour = "darkgreen",
-             size = 3) + scale_fill_discrete(breaks = c("trt1", "ctrl", "trt2")) + labs(y = "Carriage Prevalence", x = "Age (years)") 
-  
+    bind_cols(data.frame(x = x.pred)) %>%
+    ggplot(data = ., aes(x = x, y = mean)) +
+    geom_ribbon(aes(ymin = `2.5%`,
+                    ymax = `97.5%`),
+                alpha = 0.5,
+                fill = "lightskyblue") +
+    geom_line() +
+    geom_point(data = CarriageDataThesis3_,
+               aes(x = AllAges, y = p),
+               color = "red",
+               size = 3) + geom_linerange(data = CarriageDataThesis3_, aes(
+                   x = AllAges,
+                   ymin = L,
+                   ymax = U,
+                   y = p
+               )) +
+    xlab("Age") +
+    theme_classic() +
+    ylab("Prevalence") +
+    scale_x_continuous(trans = "sqrt", #we will use a square root scale on the x-axis to make the lower ages more prominent
+                       breaks = c(1:10) ^ 2) +
+    geom_point(data = data.frame(x = knots, mean = 0), color = "grey") + xlab("Age") +
+    geom_point(data = NTrang4thUnder2,
+               aes(age, carriageR),
+               colour = "darkgreen",
+               size = 3) + scale_fill_discrete(breaks = c("trt1", "ctrl", "trt2")) + labs(y = "Carriage Prevalence", x = "Age (years)") 
+
 Figure2a
 
 
@@ -207,25 +212,25 @@ Figure2a
 
 
 pred_prevalence <- 
-  pred %>%
-  mcmc_to_dt() %>%
-  pivot_wider(names_from = iteration, values_from = value, names_prefix = "post ") %>%
-  mutate(ctagegp = cut(0:99,breaks = 0:100, right=F), .before="post 1") %>%
-  select(-c(chain,parameter)) 
-  
+    pred %>%
+    mcmc_to_dt() %>%
+    pivot_wider(names_from = iteration, values_from = value, names_prefix = "post ") %>%
+    mutate(ctagegp = cut(0:99,breaks = 0:100, right=F), .before="post 1") %>%
+    select(-c(chain,parameter)) 
+
 PEI <-
-  df_contacts %>%
-  tibble() %>%
-  select(-c(X, contact_serial_no, contact_age)) %>%
-  left_join(pred_prevalence, by = "ctagegp") %>%
-  pivot_longer(cols = starts_with("post"), names_to = "posterior") %>%
-  group_by(infant_serial_no, posterior) %>% summarise(PEI = 1 - prod(1-value)) 
+    df_contacts %>%
+    tibble() %>%
+    select(-c(X, contact_serial_no, contact_age)) %>%
+    left_join(pred_prevalence, by = "ctagegp") %>%
+    pivot_longer(cols = starts_with("post"), names_to = "posterior") %>%
+    group_by(infant_serial_no, posterior) %>% summarise(PEI = 1 - prod(1-value)) 
 
 Figure2b <- 
-  PEI %>%
-  left_join(df_infants[,c("infant_serial_no","infant_pneumo_status")], by = "infant_serial_no") %>%
-  filter(!is.na(infant_pneumo_status)) %>%
-  ggplot(aes(x = as.factor(infant_pneumo_status), y = PEI)) +
+    PEI %>%
+    left_join(df_infants[,c("infant_serial_no","infant_pneumo_status")], by = "infant_serial_no") %>%
+    filter(!is.na(infant_pneumo_status)) %>%
+    ggplot(aes(x = as.factor(infant_pneumo_status), y = PEI)) +
     geom_violin(fill="lightblue") +
     geom_boxplot(width=.07, fill="orange") +
     ylab("PEI value") + xlab("") +
@@ -234,12 +239,12 @@ Figure2b <-
     theme_classic() 
 
 Figure2 <-
-  Figure2a + 
-  annotation_custom(ggplotGrob(Figure2b),
-                    xmin = 3.1,
-                    xmax = 10,
-                    ymin = .38,
-                    ymax = .85)
+    Figure2a + 
+    annotation_custom(ggplotGrob(Figure2b),
+                      xmin = 3.1,
+                      xmax = 10,
+                      ymin = .38,
+                      ymax = .85)
 ggsave("Figures/Figure2.pdf", unit = "cm", width=20, height=12)                   
 
 
@@ -250,21 +255,21 @@ ggsave("Figures/Figure2.pdf", unit = "cm", width=20, height=12)
 # Read data in preparation for Rjags -> collect these in a list called 'jdat'
 
 PEI_av <-
-  PEI %>% 
-  group_by(infant_serial_no) %>% summarise(m = mean(PEI, na.rm = T),
-                                           s = sd(PEI, na.rm = T)) %>%
-  left_join(df_infants, by = "infant_serial_no") %>%
-  filter(m>0) %>%
-  mutate(commune_id = as.factor(commune_id))
+    PEI %>% 
+    group_by(infant_serial_no) %>% summarise(m = mean(PEI, na.rm = T),
+                                             s = sd(PEI, na.rm = T)) %>%
+    left_join(df_infants, by = "infant_serial_no") %>%
+    filter(m>0) %>%
+    mutate(commune_id = as.factor(commune_id))
 
 jdat <-
-  list(
-    "pneu_carr" = PEI_av$infant_pneumo_status,
-    "mu" = PEI_av$m,
-    "sigma" = PEI_av$s,
-    "age"= PEI_av$infant_age_months,
-    "commune" = PEI_av$commune_id
-  )
+    list(
+        "pneu_carr" = PEI_av$infant_pneumo_status,
+        "mu" = PEI_av$m,
+        "sigma" = PEI_av$s,
+        "age"= PEI_av$infant_age_months,
+        "commune" = PEI_av$commune_id
+    )
 
 #Define model
 jcode <- "model{
@@ -306,10 +311,10 @@ jcode <- "model{
 #Fit and draw posterior samples
 mcmc.length = 10000
 jmod = jags.model(
-  textConnection(jcode),
-  data = jdat,
-  n.chains = 4,
-  n.adapt = 1000
+    textConnection(jcode),
+    data = jdat,
+    n.chains = 4,
+    n.adapt = 1000
 )
 update(jmod)
 jpos = coda.samples(jmod,  c("beta0","beta1","beta2","beta3"), n.iter = mcmc.length)
@@ -328,26 +333,26 @@ tidy(jpos)
 # sum(number of contacts in age group*P(carriage|age))
 
 exposure <-
-  df_contacts %>%
-  tibble() %>%
-  select(-c(X, contact_serial_no)) %>%
-  left_join(pred_prevalence, by = "ctagegp") %>%
-  pivot_longer(cols = starts_with("post"), names_to = "posterior") %>%
-  mutate(ctagegp = cut(contact_age, breaks = c(seq(0,65, by=5),100), right=F)) %>%
-  group_by(ctagegp, posterior) %>% summarise(exp = sum(value)) %>%
-  filter(!is.na(ctagegp)) %>%  #Now obtain all posterior estimates to find confidence intervals
-  group_by(posterior) %>% mutate(exp_prop = exp / sum(exp)) %>%
-  group_by(ctagegp) %>% summarise(med = median(exp_prop),
-                                  lo = quantile(exp_prop, 0.025),
-                                  hi = quantile(exp_prop, 0.975))
+    df_contacts %>%
+    tibble() %>%
+    select(-c(X, contact_serial_no)) %>%
+    left_join(pred_prevalence, by = "ctagegp") %>%
+    pivot_longer(cols = starts_with("post"), names_to = "posterior") %>%
+    mutate(ctagegp = cut(contact_age, breaks = c(seq(0,65, by=5),100), right=F)) %>%
+    group_by(ctagegp, posterior) %>% summarise(exp = sum(value)) %>%
+    filter(!is.na(ctagegp)) %>%  #Now obtain all posterior estimates to find confidence intervals
+    group_by(posterior) %>% mutate(exp_prop = exp / sum(exp)) %>%
+    group_by(ctagegp) %>% summarise(med = median(exp_prop),
+                                    lo = quantile(exp_prop, 0.025),
+                                    hi = quantile(exp_prop, 0.975))
 s = seq(0,60, by=5)
 ages = c(paste0(s,"-",s+4),"Over 65")
 
 # Plot Figure 3a (proportion of exposure due to every age group)
 
 Figure3a <- 
-  exposure %>%
-  ggplot(aes(x = ctagegp, y = med, ymin = lo, ymax = hi)) +
+    exposure %>%
+    ggplot(aes(x = ctagegp, y = med, ymin = lo, ymax = hi)) +
     geom_bar(stat = "identity", fill = "lightblue") +
     geom_linerange(color = "Orange", lwd=1.2) + 
     ylab("Proportion of exposure") + xlab("Age Group") + 
@@ -358,24 +363,24 @@ Figure3a <-
 # (with each year group in a separate bin)
 
 exposure_fine <-
-  df_contacts %>%
-  tibble() %>%
-  select(-c(X, contact_serial_no)) %>%
-  left_join(pred_prevalence, by = "ctagegp") %>%
-  pivot_longer(cols = starts_with("post"), names_to = "posterior") %>%
-  group_by(contact_age, posterior) %>% summarise(exp = sum(value)) %>%
-  filter(!is.na(exp)) %>%
-  group_by(posterior) %>% mutate(exp_prop = exp / sum(exp)) %>%
-  group_by(contact_age) %>% summarise(med = median(exp_prop),
-                                  lo = quantile(exp_prop, 0.025),
-                                  hi = quantile(exp_prop, 0.975))
+    df_contacts %>%
+    tibble() %>%
+    select(-c(X, contact_serial_no)) %>%
+    left_join(pred_prevalence, by = "ctagegp") %>%
+    pivot_longer(cols = starts_with("post"), names_to = "posterior") %>%
+    group_by(contact_age, posterior) %>% summarise(exp = sum(value)) %>%
+    filter(!is.na(exp)) %>%
+    group_by(posterior) %>% mutate(exp_prop = exp / sum(exp)) %>%
+    group_by(contact_age) %>% summarise(med = median(exp_prop),
+                                        lo = quantile(exp_prop, 0.025),
+                                        hi = quantile(exp_prop, 0.975))
 
 # Plot Figure 3b (showing the proportion of exposure due to 0-15 year olds)
 
 Figure3b <- 
-  exposure_fine %>%
-  filter(contact_age <=15) %>%
-  ggplot(aes(x = contact_age, y = med, ymin = lo, ymax = hi)) +
+    exposure_fine %>%
+    filter(contact_age <=15) %>%
+    ggplot(aes(x = contact_age, y = med, ymin = lo, ymax = hi)) +
     geom_bar(stat = "identity", fill = "lightblue") +
     geom_linerange(color = "Orange", lwd=1.2) + 
     ylab("Proportion of\nexposure") + xlab("Age (years)") + 
@@ -383,12 +388,12 @@ Figure3b <-
     theme_classic() 
 
 Figure3 <-
-  Figure3a + 
-  annotation_custom(ggplotGrob(Figure3b),
-                    xmin = 4,
-                    xmax = 14,
-                    ymin = .17,
-                    ymax = .5)
+    Figure3a + 
+    annotation_custom(ggplotGrob(Figure3b),
+                      xmin = 4,
+                      xmax = 14,
+                      ymin = .17,
+                      ymax = .5)
 ggsave("Figures/Figure3.pdf", unit = "cm", width=20, height=12)                    
 
 
